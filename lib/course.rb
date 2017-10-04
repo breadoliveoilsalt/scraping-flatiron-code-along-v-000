014b50e655936b3616f02be13ae50eadce672fb4
Tony Distinti <adistinti@gmail.com>
2017-10-04 11:26:05 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,21 @@
 class Course
+
+  attr_accessor :title, :schedule, :description 
+
+  @@all = []
+
+  def initialized
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @all.clear
+  end
+
+  
 end
 
